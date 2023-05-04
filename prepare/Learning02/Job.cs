46d3be9aa31607a2a41ffdc1a7cafe4b9c2c85e2
katsu-rai <taradancan0603@gmail.com>
2023-05-03 22:05:06 -0600

    Week3

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,24 @@
+// Class: Job
+// Attributes:
+// * _company : string
+// * _jobTitle : string
+// * _startYear : int
+// * _endYear : int
+
+// Behaviors:
+// * Display() : void
+
+using System;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
