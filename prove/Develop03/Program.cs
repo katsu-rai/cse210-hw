03d14e0fedf16105e4425bd35a9a6759312b2d3f
katsu-rai <taradancan0603@gmail.com>
2023-05-23 16:45:29 -0600

    Week6 develop

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Create scripture object
+        Console.Clear();
+        Scripture scripture = new Scripture("Isaiah", "29", "10", "0", "For the Lord hath poured out upon you the spirit of deep sleep, and hath closed your eyes: the prophets and your rulers, the seers hath he covered.");
+        string response = " ";
+
+        while (response != "quit") {
+            scripture.DisplayScripture();
+            Console.Write("Press enter to continue or \"quit\" to finish: ");
+            response = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (response) {
+                case "quit":
+                    break;
+
+                case "":
+                    if (!scripture.GetAllWordsHidden()) {
+                        scripture.HideRandomWord();
+                        scripture.HideRandomWord();
+                        scripture.HideRandomWord();
+                    }
+                    else {
+                        response = "quit";
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid response");
+                    break;
+            }
+        }
     }
 }
